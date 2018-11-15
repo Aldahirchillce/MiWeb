ce95a777cbc55ff1118e4b0e3362073060d1bc5d
Aldahir <jesus.chillce@vallegrande.edu.pe>
2018-11-15 12:04:07 -0500

    He af¿gregado mas ejercicios en Hola Mundo

--- lab01/Js/holamundo.js
+++ lab01/Js/holamundo.js
@@ -12,3 +12,19 @@ console.log("Imperial, San Vicente, Quilmana, San Luis, Nuevo Imperial");
 
 //sumar los números 100 y 500
 console.log(100 + 500)
+
+//Imprime tres nombres de paises
+console.log("Perú, Argentina, Brasil")
+
+//Imprimir mensaje de error
+console.error("Este error es de prueba... no te preocupes sano.")
+
+//Imprimir mensaje de advertencia
+console.warn("Cuidado... te estoy vigilando.")
+
+//Imprimir mensaje de información
+console.info("Este es un aviso de JavaSript")
+
+//Multiplicar tres numeros que son 10 x 25 x 105
+console.log(10 * 25 * 105)
+
