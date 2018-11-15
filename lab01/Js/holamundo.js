17ce9c9ace581bd56550b191e89bbdf252b45525
Aldahir <jesus.chillce@vallegrande.edu.pe>
2018-11-15 10:04:20 -0500

    Agregue archivo de JS

---
+++ lab01/Js/holamundo.js
@@ -0,0 +1,14 @@
+//Mi primer mensaje por consola
+console.log("Hola terricola... te escribo desde el más allá...")
+
+//Imprimir otro mensaje de prueba
+console.log("Este es otro mensaje... no te olvides de hacerlo")
+
+//Imprimir números
+console.log(3.580)
+
+//Imprimir cinco nombres de distritos
+console.log("Imperial, San Vicente, Quilmana, San Luis, Nuevo Imperial");
+
+//sumar los números 100 y 500
+console.log(100 + 500)
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 #Mis primeros pasos con JavaScript
 
 Empezaremos a conocer el lenguaje de programacion JavaScript.
+**Descripcion de laboratorio**En este laboratorio hemos vinculado un archivo JS y además hemos impreso texto y numeros en la consola de navegación.
